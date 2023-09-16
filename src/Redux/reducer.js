78fd3ac897afb06b451b9a978b98eba4728b8b6f
JohnRestrepo22 <Restrepojohn228@gmail.com>
2023-09-15 20:46:28 -0400

    Proyecto terminado

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,21 +1,37 @@
 const initialState = {
-    myFavorites:[]
+    myFavorites:[],
+    allCharactersFav: []
 }
 
 const reducer =(state = initialState,action)=> {
 switch(action.type) {
     case "ADD_FAV": 
     return{
-        ...state, 
-        myFavorites: [...state.myFavorites, action.payload]
+        ...state,
+        myFavorites: [...state.allCharactersFav,action.payload],
+        allCharactersFav: [...state.allCharactersFav, action.payload]
     };
     case "REMOVE_FAV" :
         return {
             ...state,
-            myFavorites: state.myFavorites.filter((fav) => fav.id !== Number(action.payload))
+            myFavorites: state.myFavorites.filter((fav) => fav.id !== Number(action.payload)
+            )
         }
+
+        case "FILTER":
+        const allCharactersFiltered = state.allCharactersFav.filter((char) => char.gender === action.payload)
+        return {
+            ...state,myFavorites: allCharactersFiltered
+        }
+        case "ORDER":
+            const allCharactersFavCopy = [...state.allCharactersFav]
+            return{
+                ...state,myFavorites:payload === "A"? allCharactersFavCopy.sort((a,b)=>a.id - b.id)
+                 : allCharactersFavCopy.sort((a,b) => b.id - a.id)
+            }
+        
     default:
-        return {...state}
+        return {...state};
 }
 }
 
--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,14 +1,37 @@
-import { connect } from "react-redux";
+
 import Card from "../Card/Card";
+import { useDispatch,connect } from "react-redux";
+import { filterCards,orderCards } from "../../Redux/actions";
+import { useState } from "react";
 
 
 
 const Favorites = ({ myFavorites }) => {
+  const {aux,setAux} = useState(false)
+  const dispatch = useDispatch(); 
+const handleOrder = (event) =>{
+  dispatch(orderCards(event.target.value))
+  setAux(!aux)
+}
+  const handleFilter = (event) => {
+    dispatch(filterCards(event.target.value))
+  }
   return (
     <div>
+      <select  onChange={handleOrder}>
+      <option value="A">Ascendente</option>
+      <option value="D">Descendente</option>
+      </select>
+
+      <select onChange={handleFilter}>
+      <option value="Male">Male</option>
+      <option value="Female">Female</option>
+      <option value="Genderless">Genderless</option>
+      <option value="Unknown">Unknown</option>
+      </select>
+
       {
-      myFavorites.length &&
-      myFavorites.map(
+      myFavorites?.map(
         ({
           id,
           name,
@@ -21,7 +44,7 @@ const Favorites = ({ myFavorites }) => {
           
         }) => {
           return (
-            <div className="olaaaaaa">
+            <div class>
             <Card
               key={id}
               id={id}
